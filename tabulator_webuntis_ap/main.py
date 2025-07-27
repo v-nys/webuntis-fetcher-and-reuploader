2e0a35bbd5acd2e0b83a05e4972b0ac7dace45f0
Vincent Nys <vincentnys@gmail.com>
2025-07-27 10:03:01 +0200

    Slimmed down to what I currently need.

--- tabulator_webuntis_ap/main.py
+++ tabulator_webuntis_ap/main.py
@@ -1,34 +1,30 @@
-import collections
+import tempfile
 import subprocess
 import datetime
 import functools
-import logging
-import sys
 import webuntis  # type: ignore
-from getpass import getpass
-
 import click  # type: ignore
-import yaml  # type: ignore
-
-logging.basicConfig(level=logging.WARNING)
-
-
-def _monday_preceding(date):
-    one_day = datetime.timedelta(days=1)
-    if date.weekday() == 0:
-        return date
-    else:
-        return _monday_preceding(date - one_day)
 
 
 def _coalesce_periods(list_of_periods):
+    """Merge adjacent 1-hour time slots for the same group into several-hour slots.
+
+    This is currently restricted to only work for single-class periods.
+    It is possible to make this work for multi-class periods.
+    However, that means we have to pay more attention to chronology.
+    And in that case, we should probably split those slots into one slot per class group for the calendar."""
     # should only coalesce adjacent periods of same class groups
     # so, first sort by class groups, then by time
-    sorted_periods = sorted(
-        list_of_periods,
-        key=lambda p: "+".join(sorted([klasse.name for klasse in p.klassen]))
-        + str(p.start),
-    )
+    sorted_periods = [
+        period
+        for period in sorted(
+            list_of_periods,
+            key=lambda p: "+".join(sorted([klasse.name for klasse in p.klassen]))
+            + str(p.start),
+        )
+        # NOTE: this is what restricts the function to single-group
+        if len(period.klassen) == 1
+    ]
 
     def _smoosh(ps, p):
         if ps == []:
@@ -40,80 +36,8 @@ def _coalesce_periods(list_of_periods):
             return ps + [p]
 
     coalesced = functools.reduce(_smoosh, sorted_periods, [])
+
     return coalesced
-
-
-def _collect_schedules(session, course_data):
-    """Returns a mapping from course slug to schedule. A schedule is a list of start times."""
-    all_schedules = {}
-    for course_slug, course in course_data["lesmomenten"].items():
-        start_semester = datetime.date(
-            course["vanaf"]["jaar"], course["vanaf"]["maand"], course["vanaf"]["dag"]
-        )
-        end_semester = datetime.date(
-            course["tot"]["jaar"], course["tot"]["maand"], course["tot"]["dag"]
-        )
-        all_groups = course["groepen"]
-        duration = datetime.timedelta(hours=course["uren"])
-        [webuntis_subject] = session.subjects().filter(id=[course["id"]])
-        schedule = []
-        all_schedules[course_slug] = schedule
-        time_table = session.timetable(
-            subject=webuntis_subject, start=start_semester, end=end_semester
-        )
-        time_table = _coalesce_periods(time_table)
-        for period in time_table:
-            klas_names = [klas.name for klas in period.klassen]
-            if set(all_groups) == set(klas_names):
-                if period.end - period.start == duration:
-                    schedule.append(period.start)
-    return all_schedules
-
-
-def _collect_moment_slug_contents_tuples(all_schedules, course_data):
-    """Returns a list whose elements are tuples of a datetime, a course slug and the course contents taught in that course at that time.
-    The course contents are a tree structure: leaves are strings, internal nodes are dicts from strings to lists."""
-    moment_slug_contents_tuples = []
-    for course_slug, schedule in all_schedules.items():
-        contents = course_data["lesmomenten"][course_slug]["inhouden"]
-        for counter, moment in enumerate(schedule, start=1):
-            if counter <= len(contents):  # starts at 1...
-                period_contents = contents[counter - 1]
-            else:
-                period_contents = "geen inhoud vastgelegd voor dit lesmoment"
-                logging.warning(
-                    f"geen inhoud vastgelegd voor {course_slug} op {moment}"
-                )
-            moment_slug_contents_tuples.append((moment, course_slug, period_contents))
-        if counter < len(contents):
-            logging.warning(f"minder momenten dan inhouden voor {course_slug}!")
-    return moment_slug_contents_tuples
-
-
-def enumerate_nested(lst_or_str, prefix=""):
-    if isinstance(lst_or_str, str):
-        return f"{prefix}{lst_or_str}"
-    else:
-        counter = 0
-        mapped = []
-        for elem in lst_or_str:
-            if isinstance(elem, str):
-                counter += 1
-            mapped.append(enumerate_nested(elem, f"{prefix}{counter}."))
-        return mapped
-
-
-def flatten(lst_or_atom):
-    if isinstance(lst_or_atom, list):
-        flattened = []
-        for elem in lst_or_atom:
-            if isinstance(elem, list):
-                flattened += flatten(elem)
-            else:
-                flattened.append(elem)
-        return flattened
-    else:
-        return lst_or_atom
 
 
 @click.command()
@@ -142,24 +66,44 @@ def tabulate():
         password=user_pw,
         school="AP-Hogeschool-Antwerpen",
         useragent="WebUntis Test",
-    ).login() as s:
-        for klasse in s.klassen():
-            print(klasse.name)
-    copy_result = subprocess.run(
-        [
-            "scp",
-            "-i",
-            # TODO: will want to avoid referencing this in absolute terms
-            "/home/vincentn/.ssh/id_rsa",
-            # just to see if file shows up there
-            "/home/vincentn/.dotfiles/nvim.nix",
-            f"root@{ip}:/root/nvim.nix",
-        ],
-        capture_output=True,
-        encoding="utf8",
-    )
-    print(copy_result.stdout)
-    print(copy_result.stderr)
+    ).login() as session:
+        CLOUDSYSTEMEN = 50592
+        TEST_GROUPS = ["1PRO_D1", "1PRO_D2"]
+        [webuntis_subject] = session.subjects().filter(id=[CLOUDSYSTEMEN])
+        time_table = session.timetable(
+            subject=webuntis_subject,
+            start=datetime.date(2025, 1, 1),
+            end=datetime.date(2025, 7, 1),
+        )
+        time_table = _coalesce_periods(time_table)
+        with tempfile.NamedTemporaryFile(
+            # file is written on close, but removed when context handler exits
+            # making it named allows copying it over
+            mode="w+t",
+            encoding="utf8",
+            delete_on_close=False,
+        ) as fp:
+            for period in time_table:
+                klas_names = [klas.name for klas in period.klassen]
+                for klas in klas_names:
+                    if klas in TEST_GROUPS:
+                        fp.write(
+                            f"{klas};{period.start.isoformat()};{period.end.isoformat()}\n"
+                        )
+            fp.close()
+            _copy_result = subprocess.run(
+                [
+                    "scp",
+                    "-i",
+                    # TODO: will want to avoid referencing this in absolute terms
+                    "~/.ssh/id_rsa",
+                    # just to see if file shows up there
+                    fp.name,
+                    f"root@{ip}:/root/timetable.csv",
+                ],
+                capture_output=True,
+                encoding="utf8",
+            )
 
 
 if __name__ == "__main__":
